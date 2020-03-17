eb81370bf3972de625eec625b1c78584bc046d3e
tjysdsg <tjy1018543509@163.com>
2020-03-17 20:24:38 +0800

    Update

--- wechat_red_bag_simulation/sim_competition.py
+++ wechat_red_bag_simulation/sim_competition.py
@@ -40,14 +40,20 @@ def sim_trial(n_bags: int, money: float):
 
 def sim_player_money(n_players=10, n_trials=10000, money=66.0):
     np.random.seed(1024)
-    data = np.asarray([sim_trial(n_players, money) for i in range(n_trials)]).T
+    data = np.asarray([sim_trial(n_players, money) for _ in range(n_trials)]).T
     ranks = get_ranks(data)
     player_money = np.zeros(n_players)
     lucky = np.argmax(data, axis=0)
     u, c = np.unique(lucky, return_counts=True)
+
+    # get remaining player money
     player_money[u] -= money * c
     player_money += np.sum(data, axis=1)
-    return player_money, ranks
+
+    # get number of luckiest for each player
+    n_lucky = np.empty(n_players)
+    n_lucky[u] = c
+    return player_money, ranks, n_lucky
 
 
 def get_ranks(array: np.ndarray):
@@ -58,28 +64,43 @@ def get_ranks(array: np.ndarray):
     return ranks
 
 
+def normalize(data: np.ndarray, low=-1.0, high=1.0) -> np.ndarray:
+    return low + (high - low) * (data - np.min(data)) / np.ptp(data)
+
+
 if __name__ == '__main__':
     data = [sim_player_money(np) for np in range(3, 22, 3)]
     n = len(data)
-    # for i in range(n):
-    #     data_, ranks = data[i]
-    #     fig, ax = plt.subplots()
-    #     np = data_.size
-    #     sns.barplot(list(range(np)), data_, ax=ax, palette="Blues_d")
-    #     ax.set_xlabel('player order')
-    #     ax.set_ylabel('money left')
-    #     ax.set_title('Red bag competition for {} players'.format(np))
-    #     plt.savefig('competition-{}-players.png'.format(np))
-
     for i in range(n):
-        data_, ranks = data[i]
-        mean_ranks = np.mean(ranks, axis=1)
+        bar_width = 0.35
+        data_, _, lucky = data[i]
+        # normalize data and lucky
+        data_ = normalize(data_)
+        lucky = normalize(lucky)
         fig, ax = plt.subplots()
-        sns.barplot(mean_ranks, data_, ax=ax, palette="Blues_d")
-        ax.set_xlabel('money rank')
-        ax.set_ylabel('money left')
-        ax.tick_params(axis='x', which='major', labelsize=10)
-        plt.xticks(rotation=-90)
         n_players = data_.size
-        ax.set_title('competition money w.r.t ranks; {} players'.format(n_players))
-        plt.savefig('competition-money-wrt-ranks-{}-players.png'.format(n_players))
+        labels = np.asarray(list(range(n_players)))
+        ax.bar(labels - bar_width / 2, data_, label='Remaining money', width=bar_width, color='#a6cee3')
+        ax.bar(labels + bar_width / 2, lucky, label='Number of luckiest', width=bar_width, color='#edd1cb')
+
+        ax.set_xlabel('player order')
+        ax.set_ylabel('normalized value')
+        ax.set_title('Red bag competition for {} players'.format(n_players))
+        ax.set_xticks(labels)
+        ax.set_xticklabels([str(i) for i in labels])
+        plt.legend()
+        fig.tight_layout()
+        plt.savefig('competition-{}-players.png'.format(n_players))
+
+    # for i in range(n):
+    #     data_, ranks, lucky = data[i]
+    #     mean_ranks = np.mean(ranks, axis=1)
+    #     fig, ax = plt.subplots()
+    #     sns.barplot(mean_ranks, data_, ax=ax, palette="Blues_d")
+    #     ax.set_xlabel('money rank')
+    #     ax.set_ylabel('money left')
+    #     ax.tick_params(axis='x', which='major', labelsize=10)
+    #     plt.xticks(rotation=-90)
+    #     n_players = data_.size
+    #     ax.set_title('competition money w.r.t ranks; {} players'.format(n_players))
+    #     plt.savefig('competition-money-wrt-ranks-{}-players.png'.format(n_players))
